ef8b70eace8fc68ddd02ae8df8bf2005ba9b827d
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2025-08-12 13:59:00 -0700

    start testing

--- smart-feed-fixinator.php
+++ smart-feed-fixinator.php
@@ -32,7 +32,28 @@ function gfi_force_full_content_in_feed($content) {
             $img_tag = '<p><img src="' . esc_url($featured_image_url) . '" alt="' . esc_attr(get_the_title($post)) . '" /></p>';
             $content = $img_tag . $content;
         }
+        $analytics_block = <<<XML
+            <snf:analytics>
+            <![CDATA[
+            <!-- Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-0C4N11S3HG"></script>
+            <!-- Comscore -->
+            <script>
+            var _comscore = _comscore || [];
+            _comscore.push({ c1: "2", c2: "11464276", options: { enableFirstPartyCookie: "false" } });
+            (function() {
+                var s = document.createElement("script"), el = document.getElementsByTagName("script")[0];
+                s.async = true;
+                s.src = "https://sb.scorecardresearch.com/cs/11464276/beacon.js";
+                el.parentNode.insertBefore(s, el);
+            })();
+            </script>
+            ]]>
+            </snf:analytics>
+            XML;
 
+        //$content .= "\n" . $analytics_block;
+        $content = "test";
         // Make sure the content is wrapped in CDATA for proper RSS format
         echo $content;
     }
